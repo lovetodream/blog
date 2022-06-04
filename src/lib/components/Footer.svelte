cb045c39d59dcdb018e2289f3f8b6b325f7ceabf
Timo <38291523+lovetodream@users.noreply.github.com>
2022-06-04 13:41:31 +0200

    feat: add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,19 @@
+<footer class="text-gray-400">
+	<div class="container mx-auto py-8">
+		<div
+			class="border-t border-t-gray-500 pt-2 flex justify-between text-sm leading-none font-extralight"
+		>
+			<p>
+				Copyright © 2022 <a href="https://timozacherl.com" class="text-gray-300 hover:underline"
+					>Timo Zacherl</a
+				>. All rights reserved.
+			</p>
+			<div class="flex">
+				<a href="/privacy" class="px-2 border-r border-r-gray-500 text-gray-300 hover:underline">
+					Privacy Policy
+				</a>
+				<a href="/imprint" class="pl-2 text-gray-300 hover:underline"> Imprint </a>
+			</div>
+		</div>
+	</div>
+</footer>
