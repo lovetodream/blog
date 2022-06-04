2913411105c8440acbba139ebde75779b3859da4
Timo <38291523+lovetodream@users.noreply.github.com>
2022-06-04 16:08:12 +0200

    feat: add imprint

---
+++ src/routes/imprint.svelte
@@ -0,0 +1,54 @@
+<div class="relative py-16 overflow-hidden">
+	<div class="relative px-4 sm:px-6 lg:px-8">
+		<div class="text-lg max-w-prose mx-auto">
+			<h1>
+				<span
+					class="block text-base text-center text-blue-500 font-semibold tracking-wide uppercase"
+					>Imprint / Impressum</span
+				>
+				<p class="mt-8 text-xl text-gray-400 leading-8">
+					Dieses Impressum sowie die folgenden Bestimmungen gelten für alle digitalen Angebote von
+					Swift by Timo, sofern im jeweiligen Produkt nicht anders angegeben.
+				</p>
+			</h1>
+		</div>
+		<div class="mt-6 prose prose-invert prose-blue prose-lg text-gray-400 mx-auto">
+			<h4>Angaben gemäß § 5 TMG</h4>
+			<p>
+				Swift by Timo<br />
+				Keltenring 9<br />
+				93336 Altmannstein<br />
+				Deutschland
+			</p>
+			<table class="table-auto">
+				<tr>
+					<td>Vertreten durch</td>
+					<td>Timo Zacherl</td>
+				</tr>
+				<tr>
+					<td>Telefon</td>
+					<td>+49 151 73 044 606</td>
+				</tr>
+				<tr>
+					<td>E-Mail</td>
+					<td>timo@swiftbytimo.com</td>
+				</tr>
+				<tr>
+					<td>USt.-IdNr.</td>
+					<td>
+						DE 319 505 883
+						<span class="text-xs">(gemäß §27a Umsatzsteuergesetz)</span>
+					</td>
+				</tr>
+			</table>
+			<h4>Widerspruch Webe-Mails</h4>
+			<p>
+				Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten zur
+				Übersendung von nicht ausdrücklich angeforderter Webung und Informationsmaterialien wird
+				hiermit widersprochen. Der Betreiber der Seiten behalten sich ausdrücklich rechtliche
+				Schritte im Falle der unverlangten Zustellung von Werbeinformationen, etwa durch
+				Spam-E-Mails, vor.
+			</p>
+		</div>
+	</div>
+</div>
