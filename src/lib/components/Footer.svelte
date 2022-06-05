62bc541b58bf589305517f88497525269f34243a
Timo <38291523+lovetodream@users.noreply.github.com>
2022-06-05 17:06:06 +0200

    feat: add tags to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,30 @@
+<script lang="ts">
+	import { api } from '$lib/api';
+	import type { Tags } from '@tryghost/content-api';
+
+	let tags: Tags | undefined;
+
+	api.tags.browse().then((apiTags) => {
+		tags = apiTags;
+	});
+</script>
+
 <footer class="text-gray-400 bg-black bg-opacity-75">
 	<div class="container mx-auto max-w-3xl px-4 py-8 space-y-4">
+		{#if tags}
+			<div class="flex flex-col space-y-2">
+				<h3 class="text-gray-300 text-base font-light">Tags</h3>
+				<ul class="text-sm grid grid-cols-4 gap-y-1 gap-x-4">
+					{#each tags as tag}
+						<li class="flex items-center space-x-4">
+							<a href={`/tags/${tag.slug}`} class="text-gray-400 hover:underline font-light">
+								{tag.name}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		{/if}
 		<div
 			class="border-t border-t-gray-500 pt-2 flex justify-between text-sm leading-none font-extralight"
 		>
