25d85263e13ac76f1dce94245d076dc7b604843c
Timo <38291523+lovetodream@users.noreply.github.com>
2022-06-06 19:50:38 +0200

    refactor: environment variable declaration

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,7 +1,7 @@
 export const variables = {
-    ghostUrl: import.meta.env.VITE_GHOST_URL ?? process.env['VITE_GHOST_URL'] as string,
-    ghostKey: import.meta.env.VITE_GHOST_KEY ?? process.env['VITE_GHOST_KEY'] as string,
-    serviceUrl: import.meta.env.VITE_SERVICE_URL ?? process.env['VITE_SERVICE_URL'] as string,
-    serviceKey: import.meta.env.VITE_SERVICE_KEY ?? process.env['VITE_SERVICE_KEY'] as string,
-    baseUrl: import.meta.env.VITE_BASE_URL ?? process.env['VITE_BASE_URL'] as string,
+    ghostUrl: process.env['VITE_GHOST_URL'] as string,
+    ghostKey: process.env['VITE_GHOST_KEY'] as string,
+    serviceUrl: process.env['VITE_SERVICE_URL'] as string,
+    serviceKey: process.env['VITE_SERVICE_KEY'] as string,
+    baseUrl: process.env['VITE_BASE_URL'] as string,
 }
