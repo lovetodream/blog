fa7d8e5bbd00b57d9f1a4e7fac7ba3eb4159a488
Timo <38291523+lovetodream@users.noreply.github.com>
2022-06-06 17:06:17 +0200

    feat: add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// redirect to homepage until https://github.com/sveltejs/kit/issues/4582 is fixed
+		goto('/');
+	});
+</script>
